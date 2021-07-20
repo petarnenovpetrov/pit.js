aa9a96e77862a9c415778a412b85ed60fe7894a3
Petar Nenov <petar_nenov@abv.bg>
2021-07-20 09:52:13 +0300

    chore: format code

--- src/Scroll.ts
+++ src/Scroll.ts
@@ -1,19 +1,20 @@
-
-export interface ScrollConfig{
-    position: PIXI.Point
-    mask:PIXI.Rectangle
+export interface ScrollConfig {
+  position: PIXI.Point;
+  mask: PIXI.Rectangle;
 }
 
-export interface ScrollElement<T>{
-    setData(data:T):void
-    updatePositions():void
+export interface ScrollElement<T> {
+  setData(data: T): void;
+  updatePositions(): void;
 }
 
-export class Scroll<T>{
-    protected _element:ScrollElement<T> = {} as ScrollElement<T>
-    protected _config:ScrollConfig = {} as ScrollConfig
-    init(element:ScrollElement<T>,config:ScrollConfig){
-        this._element = element
-        this._config = config
-    }
+export class Scroll<T> {
+  protected _element: ScrollElement<T> = {} as ScrollElement<T>;
+  protected _config: ScrollConfig = {} as ScrollConfig;
+  init(element: ScrollElement<T>, config: ScrollConfig) {
+    this._element = element;
+    this._config = config;
+  }
 }
+
+// useless comment
